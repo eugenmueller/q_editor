b4c69c446fb4521434f00fa6df34a5919728d899
Eugen Müller <eugen.mllr@gmail.com>
2022-12-30 23:09:07 +0100

    Add total sum footer.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,6 +3,7 @@
 class Quote < ApplicationRecord
   belongs_to :company
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   validates :name, presence: true
 
@@ -13,4 +14,8 @@ class Quote < ApplicationRecord
   # after_destroy_commit -> { broadcast_remove_to :quotes }
   # Equivalent to this notation
   broadcasts_to ->(quote) { [quote.company, :quotes] }, inserts_by: :prepend
+
+  def total_price
+    line_items.to_a.sum(&:total_price)
+  end
 end
